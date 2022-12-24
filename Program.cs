0345471db022f90df94526d9eaefd92e3a06b037
abdallasabry72 <121316424+abdallasabry72@users.noreply.github.com>
2022-12-24 11:21:59 +0200

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -4,28 +4,30 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace project_1
+namespace project_2
 {
     internal class Program
     {
         static void Main(string[] args)
         {
             Console.WriteLine("Please Enter Frist Number:");
-            int sum1 = int.Parse(Console.ReadLine());
-            Console.WriteLine("Please Enter Second Number:");
-            int sum2 = int.Parse(Console.ReadLine());
-            Console.WriteLine("Perfect Resualt:");
+            int prime = int.Parse(Console.ReadLine());
+            Console.WriteLine("Please Enter Other Number:");
+            int prime1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Prime Resualt:");
+
+            int H = 0;
+            for (int i = prime; i <= prime1; i++ )
             {
-                if (sum1 >= 0 && sum2 >= 6 && sum1 < 7)
-                    Console.WriteLine("6");
-                if (sum2 >= 28 && sum1 <= 28)
-                    Console.WriteLine("28");
-                if (sum2 >= 496 && sum1 <= 496)
-                    Console.WriteLine("496");
-                if (sum2 >= 8128 && sum1 <= 8128)
-                    Console.WriteLine("8128");
-                if (sum2 >= 33550336 && sum1 <= 33550336)
-                    Console.WriteLine("33550336");
+                for (int w = 1; w <= i; w++)
+
+                {
+                    if (i % w == 0)
+                        w++;
+                }
+                if (H == 2)
+                    Console.WriteLine("i");
+                H = 0;
             }
         }
     }
